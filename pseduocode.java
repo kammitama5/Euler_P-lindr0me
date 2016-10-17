3d4d9846f3b84e2c82936890725266b189bbd786
kammitama5 <krystal.maughan@gmail.com>
2016-10-17 08:10:00 -0700

    P@lindrome Eu!er problem
    includes pseud0code and solution

---
+++ pseduocode.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("hello world");
+    
+    //Find all palindromes
+    
+    //split each number;  first half == second half
+    //for each number, factor
+    //factor; each number from number-1 % number == 0
+    
+    
+    
+    
+    
+    
+  }
+}
